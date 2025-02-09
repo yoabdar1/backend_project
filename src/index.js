2e173c9e1533434e5f00b5d4b0e6de6859a2c8b0
Yogesh Abdar <yogeshabdar11@gmail.com>
2025-02-09 18:45:19 +0530

    connect to mongo_Atlas

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
- 
+// import mongoose from "mongoose";
+// import { DB_NAME } from "./constants";
+// ( async () => {
+//     try {
+//        mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+//     } catch (error) {
+//         console.log(error);
+//     }
+// })()
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+dotenv.config({
+  path: "./.env",
+});
+connectDB();
